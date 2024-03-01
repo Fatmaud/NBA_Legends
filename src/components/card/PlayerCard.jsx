bb68169471622c37c1bf4ae97fe7fceb71e364e7
Fatmaud <fatmaudemirci@gmail.com>
2024-03-02 00:58:22 +0100

    last touch

--- src/components/card/PlayerCard.jsx
+++ src/components/card/PlayerCard.jsx
@@ -2,9 +2,6 @@ import { useState } from "react";
 import "./Card.scss";
 
 const PlayerCard = ({ img, name, statistics }) => {
-  //!player CardContainer.jsx te, map ten object olarak olarak buraya geliyor. O yüzden karsilarken süslüyle bir tur dest. ediyrz. Bu nedenle diger propertyleri daha props u karsilarken dest. edemiyorz. Ancak asagidaki sekilde yapabilirz. Ya da parent componetten spread ile gönderiririz. Süslülerden ckarak childa gelir. Ancak bu sekilde props da dest. yaparak kullanabiliriz.
-  //const { img, name, statistics, id } = player;
-
   const [showImage, setShowImage] = useState(true);
   return (
     <div className="player-container" onClick={() => setShowImage(!showImage)}>
